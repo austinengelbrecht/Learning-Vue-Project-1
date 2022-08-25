20262159180c6d606a2b171d0577fd8498428aaf
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2022-08-24 22:04:14 -0500

    Added monster attacking player function

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const game = Vue.createApp({
       const attackDamage = Math.floor(Math.random() * (12 - 5)) + 5;
       this.monsterHealth -= attackDamage;
     },
+    attackPlayer() {
+      const attackDamage = Math.floor(Math.random() * (15 - 8)) + 8;
+      this.playerHealth -= attackDamage;
+    },
   },
 });
 game.mount("#game");
